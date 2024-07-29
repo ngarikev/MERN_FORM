a035ac69384a67acdc2a38bd26d65c0e90c3b71d
ngarikev <kevin.gary191@gmail.com>
2024-07-29 11:01:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from 'axios';
+import {
+  Button,
+  Container,
+  Form
+} from "react-bootstrap";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const data = {
+    fname: fname,
+    email: email,
+    phone: phone,
+    msg: msg,
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+   try {
+    await axios.post("http://localhost:5000", data);
+    alert('Data sent');
+    console.log(data);
+   } catch (error) {
+    console.log(error);
+   }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="mt-5">
+        <Form  className="" onSubmit={handleSubmit}>
+          <Form.Group className="mb-3 w-50 m-auto">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your name"
+              value={fname}
+              onChange={(e) => setFname(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3 w-50 m-auto">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3 w-50 m-auto">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              type="tel"
+              placeholder="Phone number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3 w-50 m-auto">
+            <Form.Label>Send message</Form.Label>
+            <Form.Control
+              as="textarea"
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+              placeholder="write here..."
+            ></Form.Control>
+            <Button className="mt-3" type="submit">
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+      </Container>
+    </>
   );
 }
 
